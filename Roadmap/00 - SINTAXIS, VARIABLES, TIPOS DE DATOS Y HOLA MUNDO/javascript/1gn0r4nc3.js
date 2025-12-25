0b42851416ea60e616c54374b712cdf97e1eaa76
1gno <164679931+1gn0r4nc3@users.noreply.github.com>
2025-12-25 17:20:03 -0500

    Mis ejemplos usando Js
    Feliz navidad! Espero esten muy bien y no molestar mucho por estas fechas. porfavor tomense su tiempo. estos son mis ejemplos y muchas  gracias por compartir estos retos. Apenas hago el primero. Pero si siento que he estado aprendiendo. gracias de nuevo y felices fiestas!

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/1gn0r4nc3.js
@@ -0,0 +1,33 @@
+//Lenguaje: JavaScript
+const JavaScript = "JavaScript";
+
+// Como hacer un comentario de una o varias lineas
+
+/*
+Documentacion oficial de JavaScript
+https://developer.mozilla.org/es/docs/Web/JavaScript
+*/
+
+//Variables y constantes
+
+const nombre = "ignoranc3";
+let edad = 22;
+
+//Datos primitivos
+
+//String
+const texto = "Hola Mundo";
+//Number
+const numero = 9;
+const decimal = 9.9;
+//Boolean
+const booleano = true;
+//Null
+const nulo = null;
+//Undefined
+let indefinido;
+//Symbol
+const simbolo = Symbol("id");
+
+//Salida por consola
+console.log(`${nombre} esta aprendiendo ${JavaScript}`);
